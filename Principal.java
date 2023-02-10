fbf020d09cfbd4e5b46638e08ddbeeb3eeacaff4
Estudiante USC <estudiante@GTACAD.USC>
2023-02-10 15:56:40 -0500

    primer comit

---
+++ Principal.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+//nombre de la clase
+public class Principal {
+
+	
+	
+	//metodo principal
+	public static void main(String[] args) {
+		
+		System.out.println("Ingrese la palabra" );
+		//clase que permite capturar datos por consola
+		Scanner 
+		sc = new Scanner (System.in);
+		
+		
+		String prueba = sc.next();
+		System.out.println(" la cantidad de le tras que tiene la palabra es : ".concat(prueba.toUpperCase());
+	}
+}
